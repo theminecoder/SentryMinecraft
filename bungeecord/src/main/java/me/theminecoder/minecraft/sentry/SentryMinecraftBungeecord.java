9fad9b7a15ddc1ad6bc73d301e4b3f42f2c2eb27
theminecoder <theminecoder.dev@gmail.com>
2020-05-11 13:35:58 +1000

    Use the new exception events I added to waterfall

--- bungeecord/src/main/java/me/theminecoder/minecraft/sentry/SentryMinecraftBungeecord.java
+++ bungeecord/src/main/java/me/theminecoder/minecraft/sentry/SentryMinecraftBungeecord.java
@@ -1,39 +1,35 @@
 package me.theminecoder.minecraft.sentry;
 
-import me.theminecoder.minecraft.sentry.delegate.DelegateEventBus;
-import me.theminecoder.minecraft.sentry.delegate.DelegatePluginManager;
+import io.github.waterfallmc.waterfall.event.ProxyExceptionEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.api.plugin.PluginManager;
-import net.md_5.bungee.event.EventBus;
+import net.md_5.bungee.event.EventHandler;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-public final class SentryMinecraftBungeecord extends Plugin {
+public final class SentryMinecraftBungeecord extends Plugin implements Listener {
 
     @Override
     public void onEnable() {
         try {
-            Field eventBusField = PluginManager.class.getDeclaredField("eventBus");
-            eventBusField.setAccessible(true);
-            eventBusField.set(this.getProxy().getPluginManager(), new DelegateEventBus((EventBus) eventBusField.get(this.getProxy().getPluginManager()), SentryMinecraft::sendIfActive));
-
-            Field pluginManagerField = this.getProxy().getClass().getDeclaredField("pluginManager");
-            pluginManagerField.setAccessible(true);
-            if (Modifier.isFinal(pluginManagerField.getModifiers())) {
-                Field modifierField = Field.class.getDeclaredField("modifiers");
-                modifierField.setAccessible(true);
-                modifierField.set(pluginManagerField, pluginManagerField.getModifiers() & ~Modifier.FINAL);
-            }
-            pluginManagerField.set(this.getProxy(), new DelegatePluginManager(this.getProxy().getPluginManager(), this.getProxy(), SentryMinecraft::sendIfActive));
-        } catch (ReflectiveOperationException e) {
-            this.getLogger().severe("Could not install proxy plugin manager! Auto-handling of event or command exceptions will not work!");
-            e.printStackTrace();
+            Class.forName("io.github.waterfallmc.waterfall.event.ProxyExceptionEvent");
+        } catch (ClassNotFoundException e) {
+            getLogger().warning("SentryMinecraft no longer supports BungeeCord plugin manager wrapping. Please use Waterfall for global exception handling");
+            return;
         }
+
+        this.getProxy().getPluginManager().registerListener(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void exceptionEvent(ProxyExceptionEvent event) {
+        this.handleException(event.getException());
+    }
+
+    private void handleException(Throwable cause) {
+        Throwable e = cause;
+        if (e.getCause() != null) { // Waterfall wraps the actual exception
+            e = e.getCause();
+        }
+        Throwable finalE = e;
+        getProxy().getScheduler().runAsync(this, () -> SentryMinecraft.sendIfActive(finalE));
     }
 }
